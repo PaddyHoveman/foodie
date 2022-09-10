8bdba8356448cee8e7d33cd5d03e7c435e43e9a0
Patrick <patrick.hoveman@clickwork.app>
2022-09-10 22:23:09 +0100

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home.component';
 import { NavComponent } from './components/nav/nav.component';
-import { ClickOutsideDirective } from './directives/click-outside.directive';
+
 import { BlogComponent } from './pages/blog.component';
+
+import { ClickOutsideDirective } from './directives/click-outside.directive';
 
 @NgModule({
   declarations: [
